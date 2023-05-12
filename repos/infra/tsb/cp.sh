e151b2e38361bc0dba219cb700593ef0cb2b2d56
Bart Van Bos <bartvanbos@gmail.com>
2023-05-12 15:44:15 +0200

    wip

--- repos/infra/tsb/cp.sh
+++ repos/infra/tsb/cp.sh
@@ -4,9 +4,11 @@
 #
 ROOT_DIR="$( cd -- "$(dirname "${0}")" >/dev/null 2>&1 ; pwd -P )"
 
-TSB_CLUSTER_CONFIG=${ROOT_DIR}/tsb-clusters.json
+TSB_CP_CLUSTERS_CONFIG=${ROOT_DIR}/tsb-cp-clusters.json
+CERT_OUTPUT_DIR=${ROOT_DIR}/output/istio-certs
 
 ACTION=${1}
+
 
 # Print info messages
 #   args:
@@ -17,8 +19,43 @@ function print_info {
   echo -e "${purpleb}${1}${end}"
 }
 
+# Generate an intermediate istio certificate signed by the self signed root certificate
+#   args:
+#     (1) cluster name
+function generate_istio_cert {
+  if [[ -f "${CERT_OUTPUT_DIR}/${1}/ca-cert.pem" ]]; then 
+    echo "File ${CERT_OUTPUT_DIR}/${1}/ca-cert.pem already exists... skipping istio certificate generation" ;
+    return ;
+  fi
+
+  mkdir -p ${CERT_OUTPUT_DIR}/${1} ;
+  openssl req -newkey rsa:4096 -sha512 -nodes \
+    -keyout ${CERT_OUTPUT_DIR}/${1}/ca-key.pem \
+    -subj "/CN=Intermediate CA/O=Istio/L=${1}" \
+    -out ${CERT_OUTPUT_DIR}/${1}/ca-cert.csr ;
+  openssl x509 -req -sha512 -days 730 -CAcreateserial \
+    -CA ${CERT_OUTPUT_DIR}/root-cert.pem \
+    -CAkey ${CERT_OUTPUT_DIR}/root-key.pem \
+    -in ${CERT_OUTPUT_DIR}/${1}/ca-cert.csr \
+    -extfile <(printf "subjectKeyIdentifier=hash\nbasicConstraints=critical,CA:true,pathlen:0\nkeyUsage=critical,digitalSignature,nonRepudiation,keyEncipherment,keyCertSign\nsubjectAltName=DNS.1:istiod.istio-system.svc") \
+    -out ${CERT_OUTPUT_DIR}/${1}/ca-cert.pem ;
+  cat ${CERT_OUTPUT_DIR}/${1}/ca-cert.pem ${CERT_OUTPUT_DIR}/root-cert.pem >> ${CERT_OUTPUT_DIR}/${1}/cert-chain.pem ;
+  cp ${CERT_OUTPUT_DIR}/root-cert.pem ${CERT_OUTPUT_DIR}/${1}/root-cert.pem ;
+  echo "New intermediate istio certificate generated at ${CERT_OUTPUT_DIR}/${1}/ca-cert.pem"
+}
+
 
 if [[ ${ACTION} = "install" ]]; then
+
+  cp_cluster_count=`jq '. | length' ${TSB_CP_CLUSTERS_CONFIG}`
+
+  for ((i=0; i<$cp_cluster_count; i++)); do
+    cp_cluster_ctx=`jq -r '.['$i'].k8s_context' ${TSB_CP_CLUSTERS_CONFIG}`
+    cp_cluster_name=`jq -r '.['$i'].cluster_name' ${TSB_CP_CLUSTERS_CONFIG}`
+    print_info "Start installation of tsb control plane in k8s cluster context '${cp_cluster_ctx}'"
+  
+  done
+
   exit 0
 fi
 
--- repos/infra/tsb/mp.sh
+++ repos/infra/tsb/mp.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/env bash
 #
-# Helper script to install tsb management plane
+# Helper script to install tsb demo management/control plane
 #
 ROOT_DIR="$( cd -- "$(dirname "${0}")" >/dev/null 2>&1 ; pwd -P )"
 
-TSB_CLUSTER_CONFIG=${ROOT_DIR}/tsb-clusters.json
+TSB_MP_CLUSTER_CONFIG=${ROOT_DIR}/tsb-mp-cluster.json
+CERT_OUTPUT_DIR=${ROOT_DIR}/output/istio-certs
 
 ACTION=${1}
+
 
 # Print info messages
 #   args:
@@ -17,8 +19,59 @@ function print_info {
   echo -e "${purpleb}${1}${end}"
 }
 
+# Generate a self signed root certificate
+function generate_root_cert {
+  mkdir -p ${CERT_OUTPUT_DIR} ;
+  if [[ -f "${CERT_OUTPUT_DIR}/root-cert.pem" ]]; then
+    echo "File ${CERT_OUTPUT_DIR}/root-cert.pem already exists... skipping root certificate generation"
+    return
+  fi
+
+  openssl req -newkey rsa:4096 -sha512 -nodes \
+    -keyout ${CERT_OUTPUT_DIR}/root-key.pem \
+    -subj "/CN=Root CA/O=Istio" \
+    -out ${CERT_OUTPUT_DIR}/root-cert.csr ;
+  openssl x509 -req -sha512 -days 3650 \
+    -signkey ${CERT_OUTPUT_DIR}/root-key.pem \
+    -in ${CERT_OUTPUT_DIR}/root-cert.csr \
+    -extfile <(printf "subjectKeyIdentifier=hash\nbasicConstraints=critical,CA:true\nkeyUsage=critical,digitalSignature,nonRepudiation,keyEncipherment,keyCertSign") \
+    -out ${CERT_OUTPUT_DIR}/root-cert.pem ;
+  echo "New root certificate generated at ${CERT_OUTPUT_DIR}/root-cert.pem"
+}
+
+# Generate an intermediate istio certificate signed by the self signed root certificate
+#   args:
+#     (1) cluster name
+function generate_istio_cert {
+  if [[ ! -f "${CERT_OUTPUT_DIR}/root-cert.pem" ]]; then generate_root_cert ; fi
+  if [[ -f "${CERT_OUTPUT_DIR}/${1}/ca-cert.pem" ]]; then 
+    echo "File ${CERT_OUTPUT_DIR}/${1}/ca-cert.pem already exists... skipping istio certificate generation" ;
+    return ;
+  fi
+
+  mkdir -p ${CERT_OUTPUT_DIR}/${1} ;
+  openssl req -newkey rsa:4096 -sha512 -nodes \
+    -keyout ${CERT_OUTPUT_DIR}/${1}/ca-key.pem \
+    -subj "/CN=Intermediate CA/O=Istio/L=${1}" \
+    -out ${CERT_OUTPUT_DIR}/${1}/ca-cert.csr ;
+  openssl x509 -req -sha512 -days 730 -CAcreateserial \
+    -CA ${CERT_OUTPUT_DIR}/root-cert.pem \
+    -CAkey ${CERT_OUTPUT_DIR}/root-key.pem \
+    -in ${CERT_OUTPUT_DIR}/${1}/ca-cert.csr \
+    -extfile <(printf "subjectKeyIdentifier=hash\nbasicConstraints=critical,CA:true,pathlen:0\nkeyUsage=critical,digitalSignature,nonRepudiation,keyEncipherment,keyCertSign\nsubjectAltName=DNS.1:istiod.istio-system.svc") \
+    -out ${CERT_OUTPUT_DIR}/${1}/ca-cert.pem ;
+  cat ${CERT_OUTPUT_DIR}/${1}/ca-cert.pem ${CERT_OUTPUT_DIR}/root-cert.pem >> ${CERT_OUTPUT_DIR}/${1}/cert-chain.pem ;
+  cp ${CERT_OUTPUT_DIR}/root-cert.pem ${CERT_OUTPUT_DIR}/${1}/root-cert.pem ;
+  echo "New intermediate istio certificate generated at ${CERT_OUTPUT_DIR}/${1}/ca-cert.pem"
+}
+
 
 if [[ ${ACTION} = "install" ]]; then
+
+  mp_cluster_ctx=`jq -r '.k8s_context' ${TSB_MP_CLUSTER_CONFIG}`
+  mp_cluster_name=`jq -r '.cluster_name' ${TSB_MP_CLUSTER_CONFIG}`
+  print_info "Start installation of tsb demo management/control plane in k8s cluster context '${mp_cluster_ctx}'"
+
   exit 0
 fi
 
