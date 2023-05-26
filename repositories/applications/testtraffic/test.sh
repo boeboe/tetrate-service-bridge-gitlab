eabdba0241bcf87d57833f932e5dba67d712fa7b
Bart Van Bos <bart@tetrate.io>
2023-05-26 22:47:49 +0200

    tweak concurrency

--- repositories/applications/testtraffic/test.sh
+++ repositories/applications/testtraffic/test.sh
@@ -5,7 +5,6 @@
 ROOT_DIR="$( cd -- "$(dirname "${0}")" >/dev/null 2>&1 ; pwd -P )"
 
 OUTPUT_DIR=${ROOT_DIR}/output/tsb
-CERTS_BASE_DIR=${ROOT_DIR}/output/ingress-certs/client/${TARGET}
 
 ACTION=${1}
 COUNT="${COUNT:-100}"
@@ -54,11 +53,12 @@ function print_command {
 #     (1) application name
 #     (2) count
 function send_curl_traffic {
+  cert_base_dir=${ROOT_DIR}/output/ingress-certs/client/${1}
   target_t1_gw_ip=$(kubectl --context mgmt get svc -n tier1-gw-${1} tier1-gw-${1} --output jsonpath='{.status.loadBalancer.ingress[0].ip}') ;
-  print_command "curl -v -H \"X-B3-Sampled: 1\" --resolve \"${1}.demo.tetrate.io:443:${target_t1_gw_ip}\" --cacert ${CERTS_BASE_DIR}/root-cert.pem --cert ${CERTS_BASE_DIR}/client1.${1}.demo.tetrate.io-cert.pem --key ${CERTS_BASE_DIR}/client1.${1}.demo.tetrate.io-key.pem \"https://${1}.demo.tetrate.io/proxy/mid-${1}.mid-${1}/proxy/back-${1}.back-${1}\""
+  print_command "curl -v -H \"X-B3-Sampled: 1\" --resolve \"${1}.demo.tetrate.io:443:${target_t1_gw_ip}\" --cacert ${cert_base_dir}/root-cert.pem --cert ${cert_base_dir}/client1.${1}.demo.tetrate.io-cert.pem --key ${cert_base_dir}/client1.${1}.demo.tetrate.io-key.pem \"https://${1}.demo.tetrate.io/proxy/mid-${1}.mid-${1}/proxy/back-${1}.back-${1}\""
 
   for ((i=0; i<${2}; i++)); do
-    curl -v -H "X-B3-Sampled: 1" --resolve "${1}.demo.tetrate.io:443:${target_t1_gw_ip}" --cacert ${CERTS_BASE_DIR}/root-cert.pem --cert ${CERTS_BASE_DIR}/client1.${1}.demo.tetrate.io-cert.pem --key ${CERTS_BASE_DIR}/client1.${1}.demo.tetrate.io-key.pem "https://${1}.demo.tetrate.io/proxy/mid-${1}.mid-${1}/proxy/back-${1}.back-${1}" ;
+    curl -v -H "X-B3-Sampled: 1" --resolve "${1}.demo.tetrate.io:443:${target_t1_gw_ip}" --cacert ${cert_base_dir}/root-cert.pem --cert ${cert_base_dir}/client1.${1}.demo.tetrate.io-cert.pem --key ${cert_base_dir}/client1.${1}.demo.tetrate.io-key.pem "https://${1}.demo.tetrate.io/proxy/mid-${1}.mid-${1}/proxy/back-${1}.back-${1}" ;
     sleep 1 ;
   done
 }
