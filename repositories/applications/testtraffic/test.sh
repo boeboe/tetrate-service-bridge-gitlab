3d42b8e31df057b5ce01463d763b82fe1fc53b63
Bart Van Bos <bart@tetrate.io>
2023-05-26 14:12:59 +0200

    added seperate test repo to send traffic

--- repositories/applications/testtraffic/test.sh
+++ repositories/applications/testtraffic/test.sh
@@ -25,13 +25,21 @@ function print_info {
   echo -e "${purpleb}${1}${end}"
 }
 
+# Print command
+#   args:
+#     (1) command
+function print_command {
+  yellowb="\033[1;33m"
+  end="\033[0m"
+  echo -e "${yellowb}${1}${end}"
+}
 
 if [[ ${ACTION} = "curl" ]]; then
 
    # Going to send test traffic using curl
   print_info "Going to send test traffic (count: ${COUNT}) to application ${TARGET} using curl" ;
   target_t1_gw_ip=$(kubectl --context mgmt get svc -n tier1-gw-${TARGET} tier1-gw-${TARGET} --output jsonpath='{.status.loadBalancer.ingress[0].ip}') ;
-  echo "curl -v -H \"X-B3-Sampled: 1\" --resolve \"${TARGET}.demo.tetrate.io:443:${target_t1_gw_ip}\" --cacert ${CERTS_BASE_DIR}/root-cert.pem --cert ${CERTS_BASE_DIR}/client1.${TARGET}.demo.tetrate.io-cert.pem --key ${CERTS_BASE_DIR}/client1.${TARGET}.demo.tetrate.io-key.pem \"https://${TARGET}.demo.tetrate.io/proxy/mid-${TARGET}.mid-${TARGET}/proxy/back-${TARGET}.back-${TARGET}\""
+  print_command "curl -v -H \"X-B3-Sampled: 1\" --resolve \"${TARGET}.demo.tetrate.io:443:${target_t1_gw_ip}\" --cacert ${CERTS_BASE_DIR}/root-cert.pem --cert ${CERTS_BASE_DIR}/client1.${TARGET}.demo.tetrate.io-cert.pem --key ${CERTS_BASE_DIR}/client1.${TARGET}.demo.tetrate.io-key.pem \"https://${TARGET}.demo.tetrate.io/proxy/mid-${TARGET}.mid-${TARGET}/proxy/back-${TARGET}.back-${TARGET}\""
 
   for ((i=0; i<${COUNT}; i++)); do
     curl -v -H "X-B3-Sampled: 1" --resolve "${TARGET}.demo.tetrate.io:443:${target_t1_gw_ip}" --cacert ${CERTS_BASE_DIR}/root-cert.pem --cert ${CERTS_BASE_DIR}/client1.${TARGET}.demo.tetrate.io-cert.pem --key ${CERTS_BASE_DIR}/client1.${TARGET}.demo.tetrate.io-key.pem "https://${TARGET}.demo.tetrate.io/proxy/mid-${TARGET}.mid-${TARGET}/proxy/back-${TARGET}.back-${TARGET}" ;
